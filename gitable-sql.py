c8a0ff932c2c42511095b5a7d07f603ef6367587
Will Morrow <duh102@gmail.com>
2015-04-13 09:39:43 -0400

    added in other-event tracking

--- gitable-sql.py
+++ gitable-sql.py
@@ -34,6 +34,21 @@ import sqlite3
 import ConfigParser
 import os.path
 
+def lCompare(item1, item2):
+  if item1[0] == item2[0]:
+    if item1[1] == item2[1]:
+      if item1[2] == item2[2]:
+        if item1[3] == item2[3]:
+          print('duplicates: '+str(item1))
+          return 0
+        else:
+          return 1 if item1[3] > item2[3] else -1
+      else:
+        return 1 if item1[2] > item2[2] else -1
+    else:
+      return int(item1[1] - item2[1])
+  else:
+    return item1[0] - item2[0]
 
 class L():
   "Anonymous container"
@@ -65,10 +80,9 @@ def dump1(u,issues, config):
   for event in w:
     issue_id = event['issue']['number']
     issue_name = event['issue']['title']
-    if not event.get('label'): continue
     created_at = secs(event['created_at'])
     action = event['event']
-    label_name = event['label']['name']
+    label_name = event['label']['name'] if 'label' in event else event['assignee']['login'] if action == 'assigned' else event['milestone']['title'] if action in ['milestoned', 'demilestoned'] else action
     user = event['actor']['login']
     milestone = event['issue']['milestone']
     if milestone != None : milestone = milestone['title']
@@ -113,9 +127,12 @@ def launchDump():
         CONSTRAINT pk_issue PRIMARY KEY (id) ON CONFLICT ABORT)''')
   conn.execute('''CREATE TABLE IF NOT EXISTS milestone(id INTEGER, name VARCHAR(128),
         CONSTRAINT pk_milestone PRIMARY KEY(id) ON CONFLICT ABORT)''')
+
+  #unsure if ignoring duplicate event tuples is a good idea, but the unique information is pretty much all we care about
+  #duplicates aren't meaningful, so I guess it 's ok
   conn.execute('''CREATE TABLE IF NOT EXISTS event(issueID INTEGER NOT NULL, time DATETIME NOT NULL, action VARCHAR(128),
         label VARCHAR(128), user VARCHAR(128), milestone INTEGER,
-        CONSTRAINT pk_event PRIMARY KEY (issueID, time, action, label) ON CONFLICT ABORT,
+        CONSTRAINT pk_event PRIMARY KEY (issueID, time, action, label) ON CONFLICT IGNORE,
         CONSTRAINT fk_event_issue FOREIGN KEY (issueID) REFERENCES issue(id) ON UPDATE CASCADE ON DELETE CASCADE,
         CONSTRAINT fk_event_milestone FOREIGN KEY (milestone) REFERENCES milestone(id) ON UPDATE CASCADE ON DELETE CASCADE)''')
   nameNum = 1
@@ -144,18 +161,29 @@ def launchDump():
       if not event.user in nameMap:
         nameMap[event.user] = config.get('options','repo')+'/user'+str(nameNum)
         nameNum+=1
+      if event.action == 'assigned' and not event.what in nameMap:
+        nameMap[event.what] = config.get('options','repo')+'/user'+str(nameNum)
+        nameNum+=1
       if 'milestone' in event.__dict__ and not event.milestone in milestoneMap:
         milestoneMap[event.milestone] = milestoneNum
         milestoneTuples.append([milestoneMap[event.milestone], event.milestone])
         milestoneNum += 1
-      eventTuples.append([issue, event.when, event.action, event.what, nameMap[event.user], milestoneMap[event.milestone] if 'milestone' in event.__dict__ else None])
+      if event.action in ['milestoned','demilestoned'] and not event.what in milestoneMap:
+        milestoneMap[event.what] = milestoneNum
+        milestoneTuples.append([milestoneMap[event.what], event.what])
+        milestoneNum += 1
+      eventTuples.append([issue, event.when, event.action, nameMap[event.what] if event.action == 'assigned' else milestoneMap[event.what] if event.action in ['milestoned','demilestoned'] else event.what, nameMap[event.user], milestoneMap[event.milestone] if 'milestone' in event.__dict__ else None])
     print('')
-  conn.executemany('INSERT INTO issue VALUES (?,?)', issueTuples)
-  conn.commit()
-  conn.executemany('INSERT INTO milestone VALUES (?,?)', milestoneTuples)
-  conn.commit()
-  conn.executemany('INSERT INTO event VALUES (?, ?, ?, ?, ?, ?)', eventTuples)
-  conn.commit()
+  sorted(eventTuples, cmp=lCompare)
+  try:
+    conn.executemany('INSERT INTO issue VALUES (?,?)', issueTuples)
+    conn.commit()
+    conn.executemany('INSERT INTO milestone VALUES (?,?)', milestoneTuples)
+    conn.commit()
+    conn.executemany('INSERT INTO event VALUES (?, ?, ?, ?, ?, ?)', eventTuples)
+    conn.commit()
+  except sqlite3.Error as er:
+    print(er)
 
   conn.close()
     
